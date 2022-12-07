e1207cc6b54032157d234f9a6944b9d37144cde2
Bergson Lima <bergsonsud@gmail.com>
2022-12-07 01:08:13 -0300

    added validators

---
+++ spec/validators/cns_validator_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+require 'active_model'
+require File.expand_path('app/models/cns')
+require File.expand_path('app/validators/cns_validator')
+
+class CnsValidatable
+  include ActiveModel::Validations
+  attr_accessor :cns
+
+  validates :cns, cns: true
+end
+
+RSpec.describe CnsValidatable do
+  it 'validates if cns is valid' do
+    subject.cns = '255505685780007'
+    expect(subject.valid?).to be(true)
+  end
+
+  it 'validates if cns is invalid' do
+    subject.cns = '123987'
+    expect(subject.valid?).to be(false)
+    expect(subject.errors['cns']).to eq ['is invalid']
+  end
+end
