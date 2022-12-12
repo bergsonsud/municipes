917ddffbce81d50196018a55a3fd27559e20a0c6
Bergson Lima <bergsonsud@gmail.com>
2022-12-12 14:49:20 -0300

    added validations

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Address do
   describe 'relations' do
     it { is_expected.to belong_to(:municipe) }
   end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:street) }
+    it { is_expected.to validate_presence_of(:zipcode) }
+    it { is_expected.to validate_presence_of(:neighborhood) }
+    it { is_expected.to validate_presence_of(:city) }
+    it { is_expected.to validate_presence_of(:uf) }
+  end
 end
